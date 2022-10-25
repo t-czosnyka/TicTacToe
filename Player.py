34ad084a04632fc9367e2d6d9b3c4ee33ba67da1
T.Czosnyka <t.czosnyka90@gmail.com>
2022-10-25 13:04:30 +0200

    more tactics added

--- Player.py
+++ Player.py
@@ -18,13 +18,17 @@ class Player:
     def get_positions(self, board):
         choices = list()
         prio = 100              # action priority - smaller -> more important
-        if len(board.free_fields) >= 7:
+        corners = [(0, 0), (1, 1), (0, board.size-1), (board.size-1, 0), (board.size-1, board.size-1)]
+        if len(board.free_fields) == 9: #first player - center or corners
             prio = 10
+            choices = choices = corners + [(1, 1)]
+        elif len(board.free_fields) == 8: #second player - take center if free or corners
             if board.check_empty(1, 1):
                 choices = [(1, 1)]
             else:
-                choices = [(0, 0), (0, board.size-1), (board.size-1, 0), (board.size-1, board.size-1)]
-        elif len(board.free_fields) < 7:
+                choices = corners
+            prio = 10
+        elif len(board.free_fields) < 8:
             possible_choices = list()            # Evaluate situation on board
             for i in range(1, 9):
                 line = board.check_line(i)
@@ -36,15 +40,28 @@ class Player:
                 if line_1[self.number-1] == 2 and line_1[2 - self.number] == 0:         # Vicotry possible prio = 1
                     if prio > 1:
                         choices.clear()
-                    prio = 1
-                    choices = choices + line_1[2]
-                    #print("Prio 1 found")
+                        prio = 1
+                    if prio == 1:
+                        choices = choices + line_1[2]
+                    print("Prio 1 found")
                 elif line_1[self.number-1] == 0 and line_1[2 - self.number] == 2 and prio >= 2:  # Block defeat prio = 2
                     if prio > 2:
                         choices.clear()
-                    prio = 2
-                    choices = choices + line_1[2]
-                    #print("Prio 2 found")
+                        prio = 2
+                    if prio == 2:
+                        choices = choices + line_1[2]
+                    print("Prio 2 found")
+                elif line_1[self.number-1] == 1 and line_1[2 - self.number] == 0:      #Play in a most promising line
+                    if prio > 3:
+                        choices.clear()
+                        prio = 3
+                    if prio == 3:
+                        line_corners = [value for value in line_1[2] if value in corners]  #find free corner to play
+                        if len(line_corners) > 0:
+                            choices = choices + line_corners
+                        else:
+                            choices = choices + line_1[2]
+                    print("Prio 3 found")
             print("choices", choices)
         if prio < 100:
             print("Not random played.")
