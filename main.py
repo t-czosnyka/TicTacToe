fe865d6b197726d0add151585c0e35b647ba4520
T.Czosnyka <t.czosnyka90@gmail.com>
2023-03-05 19:27:22 +0100

    counting results added

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
 from Game import Game
 
+
 def test_game():
+    # test computer vs MinMax Computer game without display, return winner number
     game = Game()
-    game.start(False, "3")
+    return game.start(False, "3")
+
 
 def main():
+    # main game function
     game = Game()
     game.start()
 
 
-# main game function
 if __name__ == '__main__':
-    #main()
-    for _ in range(101):
-        test_game()
-
+    main()
+    # testing 100 games
+    # draws = 0
+    # for _ in range(100):
+    #     if test_game() == 0:
+    #         draws += 1
+    # print(draws)
