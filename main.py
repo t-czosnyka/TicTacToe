a29a3974d9c018621fab2ab9b611b16100bf6601
T.Czosnyka <t.czosnyka90@gmail.com>
2023-03-05 19:04:59 +0100

    test function added, for testing multiple games

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 from Game import Game
 
+def test_game():
+    game = Game()
+    game.start(False, "3")
 
 def main():
     game = Game()
-    winner = game.start()
+    game.start()
 
 
 # main game function
 if __name__ == '__main__':
-    main()
+    #main()
+    for _ in range(101):
+        test_game()
 
