f3e2e69517189804496ee551f2a24c1e7a792084
T.Czosnyka <t.czosnyka90@gmail.com>
2023-03-06 16:59:42 +0100

    function get_players_added, game_type constants added

--- Game.py
+++ Game.py
@@ -21,45 +21,26 @@ class Game:
         # Constants "X" player is 1, "O" player is 2
         self.X_PLAYER = 1
         self.O_PLAYER = 2
+        # Constants Game types
+        self.GAME_TYPE_H_VS_H = "1"  # Human vs Human
+        self.GAME_TYPE_H_VS_C = "2"  # Human vs Computer
+        self.GAME_TYPE_C_VS_MM = "3"  # Computer vs MinMax Computer
+        self.GAME_TYPE_H_VS_MM = "4"  # Human vs MinMax Computer
+        self.GAME_TYPES = [self.GAME_TYPE_H_VS_H, self.GAME_TYPE_H_VS_C, self.GAME_TYPE_C_VS_MM, self.GAME_TYPE_H_VS_MM]
 
-    def start(self, display_game=True, game_type_in=" "):
+    def start(self, display_game=True, game_type_in=" ") -> int:
         # main game function, default - choose game type from cli, if game type is not empty - choice will be skipped
+        # returns number of player who has won the game or 0 if it ended in a draw
         # display game -if true game is displayed in cli with wait times
         # get game type
         type_ok, game_type = self.get_game_type(game_type_in)
         if not type_ok:
             raise Exception("Wrong game type passed as argument")
-        # correct game type - start game
-        # randomize player marks "X" and "O"
-        player_marks = random.sample([self.X_PLAYER, self.O_PLAYER], 2)
-        # Game type 1: Human vs Human
-        if game_type == "1":
-            # game is always displayed
+        # all games involving human player are displayed
+        if game_type != self.GAME_TYPE_C_VS_MM:
             display_game = True
-            # Create players with random marks
-            self.players.append(HumanPlayer(player_marks[0]))
-            self.players.append(HumanPlayer(player_marks[1]))
-        # Game type 2: Human vs Regular Computer
-        elif game_type == "2":
-            # game is always displayed
-            display_game = True
-            # Create players with random marks
-            self.players.append(ComputerPlayer(player_marks[0]))
-            self.players.append(HumanPlayer(player_marks[1]))
-        # Game type 3: MinMax Computer vs Regular Computer
-        elif game_type == "3":
-            # Create players with random marks
-            self.players.append(MinMaxComputerPlayer(player_marks[0], display_game))
-            self.players.append(ComputerPlayer(player_marks[1], display_game))
-        # Game type 4: Human vs MinMax Computer
-        elif game_type == "4":
-            # game is always displayed
-            display_game = True
-            # Create players with random marks
-            self.players.append(MinMaxComputerPlayer(player_marks[0]))
-            self.players.append(HumanPlayer(player_marks[1]))
-        # randomize which player starts
-        random.shuffle(self.players)
+        # correct game type - create players
+        self.get_players(game_type, display_game)
         os.system('cls')
         if display_game:
             print("\n")
@@ -91,14 +72,15 @@ class Game:
                            + str(self.board))
         # return winner
         return self.board.winner
-    @staticmethod
-    def get_game_type(game_type_in):
+
+    def get_game_type(self, game_type_in) -> (bool, str):
         # get game type as function argument or inserted by user
+        # return True if game type is ok + game type as str
         type_ok = False
         game_type = ""
         # game type inserted as argument
         if game_type_in != " ":
-            if game_type_in in ["1", "2", "3", "4"]:
+            if game_type_in in self.GAME_TYPES:
                 game_type = game_type_in
                 type_ok = True
             return type_ok, game_type
@@ -113,9 +95,38 @@ class Game:
                    4 - Player vs MinMaxComputer\n")
             game_type = input()
             os.system('cls')
-            if game_type in ["1", "2", "3", "4"]:
+            if game_type in self.GAME_TYPES:
                 type_ok = True
             else:
                 print("Wrong game type.")
         return type_ok, game_type
 
+    def get_players(self, game_type, display_game):
+        # Create list of player objects based on game type
+        # randomize player marks "X" and "O"
+        player_marks = random.sample([self.X_PLAYER, self.O_PLAYER], 2)
+        # Game type 1: Human vs Human
+        if game_type == self.GAME_TYPE_H_VS_H:
+            # game is always displayed
+            # Create players with random marks
+            self.players.append(HumanPlayer(player_marks[0]))
+            self.players.append(HumanPlayer(player_marks[1]))
+        # Game type 2: Human vs Regular Computer
+        elif game_type == self.GAME_TYPE_H_VS_C:
+            # game is always displayed
+            # Create players with random marks
+            self.players.append(ComputerPlayer(player_marks[0]))
+            self.players.append(HumanPlayer(player_marks[1]))
+        # Game type 3: Regular Computer vs MinMax Computer
+        elif game_type == self.GAME_TYPE_C_VS_MM:
+            # Create players with random marks
+            self.players.append(MinMaxComputerPlayer(player_marks[0], display_game))
+            self.players.append(ComputerPlayer(player_marks[1], display_game))
+        # Game type 4: Human vs MinMax Computer
+        elif game_type == self.GAME_TYPE_H_VS_MM:
+            # game is always displayed
+            # Create players with random marks
+            self.players.append(MinMaxComputerPlayer(player_marks[0]))
+            self.players.append(HumanPlayer(player_marks[1]))
+        # randomize which player starts
+        random.shuffle(self.players)
